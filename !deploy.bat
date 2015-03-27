e6be3fbc7133b6987b41f1b5169b3122a9f7ddfb
Krzysztof Kluczek <krzysiek@devkk.net>
2015-03-27 15:49:29 +0100

    - Fixed deployment BAT file.

--- !deploy.bat
+++ !deploy.bat
@@ -1,8 +1,43 @@
 @echo off
-copy src\base C:\!DLL\include\base
-copy src\base.h C:\!DLL\include
-copy lib C:\!DLL\lib
+
+echo.
+echo Deploying library to following locations:
+echo.
+echo     USER_INCLUDE_PATH = %USER_INCLUDE_PATH%
+echo     USER_LIBRARY_PATH = %USER_LIBRARY_PATH%
+echo.
+echo.
+
+
+if "%USER_INCLUDE_PATH%" == "" goto ERROR
+if "%USER_LIBRARY_PATH%" == "" goto ERROR
+
+
+
+copy src\base %USER_INCLUDE_PATH%\base
+copy src\base.h %USER_INCLUDE_PATH%
+copy lib %USER_LIBRARY_PATH%
+
+
 
 if "%1"=="np" goto END
+echo.
+echo.
 pause
+
+
+goto END
+
+
+
+
+:ERROR
+
+echo ERROR! - Please set correctly the above enviroment variables!
+echo.
+echo.
+
+pause
+
+
 :END
